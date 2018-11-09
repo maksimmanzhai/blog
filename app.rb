3455a8b6b18052af3f4964254af449596bdb358d
maksimmanzhai <bender23max@gmail.com>
2018-11-09 13:52:39 +0300

    Add migration

--- app.rb
+++ app.rb
@@ -6,6 +6,14 @@ require 'sinatra/activerecord'
 
 set :database, "sqlite3:blog.db"
 
+class User < ActiveRecord::Base 
+	validates :name, presence: true, length: { minimum: 3 }
+end
+
+class Message < ActiveRecord::Base 
+	validates :text, presence: true, length: { maximum: 140 }
+end
+
 get '/' do
 	erb :posts
 end
---
+++ db/migrate/20181109105035_Message.rb
@@ -0,0 +1,4 @@
+class Message < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
